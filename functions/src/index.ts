bbcc09d0e93b54a954d60eb026843c7da2dbfb53
Atsushi Nakatsugawa <nakatsugawa@AtsushinoMac-mini.local>
2023-10-18 16:02:00 +0900

    update code

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,21 +1,25 @@
+// 必要なモジュールやライブラリをインポート
 import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+// loggerのインポートがコメントアウトされています。現在は使用していません。
+// import * as logger from "firebase-functions/logger";
 import {BlastEngine, Mail} from "blastengine";
 
+// 環境変数からBlastEngineのユーザーIDとAPIキーを取得
+const userId = process.env.BLASTENGINE_USER_ID!;
+const apiKey = process.env.BLASTENGINE_API_KEY!;
+
+// BlastEngineの設定を初期化
+new BlastEngine(userId, apiKey);
+
+// メールを送信する関数をエクスポート（公開）します。
 export const sendMail = onRequest(async (request, response) => {
+  // リクエストから本文を取得
   const {body} = request;
-  const userId = process.env.BLASTENGINE_USER_ID;
-  const apiKey = process.env.BLASTENGINE_API_KEY;
-  if (userId === undefined) {
-    logger.error("BLASTENGINE_USER_ID is undefined");
-    return;
-  }
-  if (apiKey === undefined) {
-    logger.error("BLASTENGINE_API_KEY is undefined");
-    return;
-  }
-  new BlastEngine(userId, apiKey);
+  
+  // 新しいメールのインスタンスを作成
   const mail = new Mail();
+
+  // 送信するメールのテキスト内容を定義
   const text = `__USERNAME__様
   お問い合わせいただきありがとうございます。内容を確認し、追ってご連絡いたします。
   
@@ -26,16 +30,22 @@ export const sendMail = onRequest(async (request, response) => {
   お問い合わせ内容：
   __MESSAGE__
   `;
+
+  // メールの各設定を行います。
   mail
-    .setFrom("info@opendata.jp", "管理者")
-    .setSubject("お問い合わせありがとうございます")
-    .addCc("atsushi@moongift.co.jp")
-    .addTo(body.email, {
+    .setFrom("info@opendata.jp", "管理者")  // 送信者のアドレスと名前を設定
+    .setSubject("お問い合わせありがとうございます")  // メールの件名を設定
+    .addCc("atsushi@moongift.co.jp")  // CC（同報送信）のアドレスを追加
+    .addTo(body.email, { // 宛先と、テキスト内のプレースホルダを設定
       USERNAME: body.name,
       COMPANY: body.company,
       MESSAGE: body.message})
-    .setText(text)
-    .setEncode("UTF-8");
+    .setText(text)  // 上で定義したテキスト内容をセット
+    .setEncode("UTF-8");  // 文字エンコードをUTF-8に設定
+
+  // メールを送信
   await mail.send();
+
+  // レスポンスとして、配信IDをJSON形式で返します。
   response.status(200).json({delivery_id: mail.deliveryId || ""});
 });
